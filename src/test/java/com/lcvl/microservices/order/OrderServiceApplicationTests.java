cd566cdc85cbf0a5cd1ab270f3ca32b222383fb5
LuiscVillarrealL <luiscvillarreall@gmail.com>
2024-12-27 15:37:31 +0000

    kafka producer with test config

--- src/test/java/com/lcvl/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/lcvl/microservices/order/OrderServiceApplicationTests.java
@@ -1,20 +1,26 @@
 package com.lcvl.microservices.order;
 
 
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
+import com.lcvl.microservices.commons.order.event.OrderPlacedEvent;
 import com.lcvl.microservices.order.stubs.InventoryClientStub;
 
-import static org.junit.Assert.assertThat;
-
-import org.hamcrest.Matchers;
 import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
@@ -26,6 +32,9 @@ class OrderServiceApplicationTests {
     static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
     @LocalServerPort
     private Integer port;
+    
+    @Autowired
+    private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     @BeforeEach
     void setup() {
@@ -36,15 +45,24 @@ class OrderServiceApplicationTests {
     static {
         mySQLContainer.start();
     }
+    
 
     @Test
     void shouldSubmitOrder() {
         String submitOrderJson = """
-                {
-                     "skuCode": "iphone_15",
-                     "price": 1000,
-                     "quantity": 1
-                }
+               {
+    "skuCode": "iphone_15",
+    "quantity": 1,
+    "price" : 1000,
+
+    "userDetails": {
+    "email": "test@kafka.com",
+    "firstName": "test",
+    "lastName": "kafka"
+  }
+
+
+}
                 """;
 
         InventoryClientStub.stubInventoryCall("iphone_15", 1);
